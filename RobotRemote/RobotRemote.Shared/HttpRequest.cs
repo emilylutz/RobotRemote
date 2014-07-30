350cd7c365ffc14bfbe212ccfcd3996fa000cee9
Emily Lutz <t-emlutz@microsoft.com>
2014-07-29 19:36:27 -0700

    Initial commit

---
+++ RobotRemote/RobotRemote.Shared/HttpRequest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RobotRemote
+{
+    class HttpRequest
+    {
+        private string node_host = "robotcontrol.azurewebsites.net";//"wof-nodebot.azurewebsites.net"; //"robotcontrol.azurewebsites.net"; //CHANGE BACK TO "[your host]"
+        string json;
+        public HttpRequest(string body)
+        {
+            json = body;
+        }
+        public void sendInstructions()
+        {
+            string sURL = "http://" + node_host + "/robot/list";
+            //Uri url = new Uri(sURL);
+
+            try
+            {
+                // Create the web request object 
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(sURL);
+                webRequest.Method = "POST";
+                webRequest.ContentType = "application/json";//;charset=utf-8";
+                webRequest.Accept = "application/json";
+                //webRequest.Host = node_host;
+                webRequest.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), webRequest);
+            }
+            catch { }
+        }
+        void GetRequestStreamCallback(IAsyncResult asynchronousResult)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)asynchronousResult.AsyncState;
+            // End the stream request operation 
+            Stream postStream = webRequest.EndGetRequestStream(asynchronousResult);
+
+            // Create the post data 
+
+            //var input = Newtonsoft.Json.JsonConvert.SerializeObject(postData);
+            byte[] byteArray = Encoding.UTF8.GetBytes(json);
+
+            // Add the post data to the web request 
+            postStream.Write(byteArray, 0, byteArray.Length);
+            postStream.Dispose();
+
+            // Start the web request 
+            webRequest.BeginGetResponse(new AsyncCallback(GetResponseCallback), webRequest);
+        }
+        void GetResponseCallback(IAsyncResult asynchronousResult)
+        {
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)asynchronousResult.AsyncState;
+                // End the get response operation 
+                HttpWebResponse response = (HttpWebResponse)webRequest.EndGetResponse(asynchronousResult);
+                Stream streamResponse = response.GetResponseStream();
+                StreamReader streamReader = new StreamReader(streamResponse);
+                var responses = streamReader.ReadToEnd();
+                String stringResponses = (String)responses;
+                streamResponse.Dispose();
+                streamReader.Dispose();
+                response.Dispose();
+
+            }
+            catch (WebException e)
+            {
+                // Error treatment 
+                // ... 
+            }
+        } 
+    }
+}
---
+++ RobotRemote/RobotRemote.Shared/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+//Add references to System.Web.Extensions
+namespace RobotRemote
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// 
+
+    public sealed partial class MainPage : Page
+    {
+        private string fwd = "{\"commandList\":[{\"angle\":0,\"distance\":1}]}";
+        private string back = "{\"commandList\":[{\"angle\":180,\"distance\":1}]}";
+        private string right = "{\"commandList\":[{\"angle\":5,\"distance\":0}]}";
+        private string left = "{\"commandList\":[{\"angle\":5,\"distance\":0}]}";
+        private string stop = "{\"commandList\":[{\"angle\":0,\"distance\":0}]}";
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void stopButtonClicked(object sender, RoutedEventArgs e)
+        {
+            HttpRequest request = new HttpRequest(stop);
+            request.sendInstructions();
+        }
+
+        private void upButtonClicked(object sender, RoutedEventArgs e)
+        {
+            HttpRequest request = new HttpRequest(fwd);
+            request.sendInstructions();
+
+        }
+
+        private void leftButtonClicked(object sender, RoutedEventArgs e)
+        {
+            HttpRequest request = new HttpRequest(left);
+            request.sendInstructions();
+
+        }
+
+        private void rightButtonClicked(object sender, RoutedEventArgs e)
+        {
+            HttpRequest request = new HttpRequest(right);
+            request.sendInstructions();
+
+        }
+
+        private void downButtonClicked(object sender, RoutedEventArgs e)
+        {
+            HttpRequest request = new HttpRequest(back);
+            request.sendInstructions();
+
+        }
+    }
+}
